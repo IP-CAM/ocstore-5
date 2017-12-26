a403116df242c158bbfa23dcdc9cbefa41751528
c2gdnb <neonx2007@gmail.com>
2017-12-26 11:32:51 +0300

    from timeweb

---
+++ admin/language/en-gb/extension/payment/lifepay_cloud_print.php
@@ -0,0 +1,24 @@
+<?php
+$_['heading_title'] = 'LIFE PAY Cloud Print Module';
+
+$_['text_extension'] = 'Extensions';
+$_['text_lifepay_cloud_print'] = '<a target="_BLANK" href="https://www.life-pay.ru"><img src="view/image/payment/logo_lifepay_94_25.png" alt="LIFE PAY" title="LIFE PAY"></a>';
+$_['text_success'] = 'Success: You have modified LIFE PAY account details!';
+
+$_['button_save']    = 'Save changes';
+$_['button_cancel']    = 'Discard changes';
+
+$_['status_enabled']    = 'Enabled';
+$_['status_disabled']   = 'Disabled';
+
+$_['entry_lifepay_status']   = 'Module status';
+$_['entry_lifepay_testmode']   = 'Test mode';
+$_['entry_lifepay_login']   = 'LIFE PAY login';
+$_['entry_lifepay_api_key']   = 'API key';
+$_['entry_lifepay_mode']   = 'Print / send check mode';
+$_['entry_lifepay_target_serial']   = 'Printer serial number.';
+$_['entry_lifepay_target_serial_help']   = 'Serial number of the printer on which you want to print a fiscal receipt. If not specified, the check will be fiscalized on one of the printers.';
+
+$_['error_permission'] = 'Warning: You do not have permission to modify settings LIFE PAY Cloud Print Module';
+$_['error_login'] = 'LIFE PAY login Required!';
+$_['error_api_key'] = 'LIFE PAY API Key Required!';
